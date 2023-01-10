ee69f4a323da064e18c382a6678ef86693b05053
고동현 <dhjk35@naver.com>
2023-01-11 02:09:16 +0900

    refactor(server): api 경로 수정

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ server.use(middlewares);
 server.use(
   jsonServer.rewriter({
     "/api/*": "/$1",
-    "/blog/:resource/:id/show": "/:resource/:id",
+    "/capa/:requests/:id/show": "/:requests/:id",
   })
 );
 server.use(router);
